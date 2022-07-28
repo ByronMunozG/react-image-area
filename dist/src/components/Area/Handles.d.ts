b9b2fce39ba3a38187eb178f26ada6c9037de589
Byron Muñoz G <byron.munoz.godoy@gmail.com>
2022-07-28 00:30:28 -0400

    chore: remove dist directory

--- dist/src/components/Area/Handles.d.ts
+++
@@ -1,3 +0,0 @@
-declare const Handles: () => JSX.Element;
-export default Handles;
-//# sourceMappingURL=Handles.d.ts.map
--- dist/src/components/AreaSelector/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const isSubElement: (el: any, check: (el: any) => boolean) => boolean;
-//# sourceMappingURL=utils.d.ts.map
