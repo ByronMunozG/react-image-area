fe9b7452e7bbb8bf587b2b06a2c81f41fb0ebd3b
ByronMunozG <byron.munoz.godoy@gmail.com>
2022-07-27 22:58:31 -0400

    chore(vite.config.ts): add terser, add new prerelease branch in semantic-release

--- release.config.js
+++ release.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-    branches: [
-      'main'
-    ],
+    branches: ['main', { name: 'beta', prerelease: true }],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGELOG.md'
-        }
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'dist/**'],
-          message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ]
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md'
+            }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'dist/**'],
+                message:
+                    'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ]
     ]
-  }
+};
